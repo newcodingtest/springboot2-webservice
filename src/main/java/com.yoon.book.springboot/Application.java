d8e78c2d75f34e5b5948a9a00bb75e8b7a427978
newcodingtest <pulpul8282@naver.com>
2021-10-04 20:28:24 +0900

    테스트 코드 작성완료 +오류문제 해결 secure=false를 추가해줬음

---
+++ src/main/java/com.yoon.book.springboot/Application.java
@@ -0,0 +1,12 @@
+package com.yoon.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// @SpringBootApplication이 있는 위치부터 스프링이 설정일 읽어가기 때문에 항상 프로젝트의 최상단에 위치해야한다.
+public class Application {
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args); //내장 WAS를 실행시키는 것
+    }
+}
